7ef1d079d6a5101d85e84d261c78d5bad2cf1737
agubucu <aemirozbek@gmail.com>
2023-07-24 00:00:10 +0300

    Timer Function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import { useState } from "react";
 
 
 function App() {
-
   const [breakLength, setBreakLength] = useState(5)
   const [sessionLength, setSessionLength] = useState(25)
+  const [timeLeft, setTimeLeft] = useState({minutes: sessionLength*60, seconds: 0})
+  const [timer, setTimer] = useState(0)
+  let start
 
-  function handleStartStop() {}
-
+  function handleStartStop(sessionLengthParameter) {
+    setTimeLeft(sessionLengthParameter)
+    start = Date.now()
+    timerFunction(sessionLengthParameter)
+  }
+  function timerFunction(sessionLengthParameter) {
+    setInterval(() => {
+      setTimeLeft(sessionLengthParameter - (((Date.now() - start)/1000).toFixed(0)))
+    }, 1000);
+  }
+console.log(timeLeft)
 
   return (
     <div className="App">
@@ -22,7 +33,9 @@ function App() {
 
       <div id="timer-label">{"Session"}</div>
       <div id="time-left">{sessionLength}</div>
-      <button id="start_stop" onClick={handleStartStop}>Start Stop</button>
+
+
+      <button id="start_stop" onClick={()=>handleStartStop(sessionLength)}>Start Stop</button>
       <button id="reset" onClick={()=>{setBreakLength(5); setSessionLength(25)}}>reset</button>
     </div>
   );
