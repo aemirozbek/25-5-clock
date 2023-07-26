4a5803ba8f52a4c71ae33e528882b9086020cf07
Emir <98756105+agubucu@users.noreply.github.com>
2023-07-26 10:16:06 +0300

    Accurate interval (#1)
    * Fully functional, method of countdown is changed.
    
    * fixed a little "bug"

--- src/App.js
+++ src/App.js
@@ -1,129 +1,170 @@
+// Thanks to Bergi (https://stackoverflow.com/users/1048572/bergi) from stackoverflow, who posted the interval drift solution in:
+// https://stackoverflow.com/questions/29971898/how-to-create-an-accurate-timer-in-javascript
+
 import { useState, useRef } from "react";
 
-  let running = false
-  let switched = false
-  let currentState = "Session"
-  let start, time, minutes, seconds, totalTimeLeft
+let running = false;
+let switched = false;
+let currentState = "Session";
 
 function App() {
-  const [breakLength, setBreakLength] = useState(5)
-  const [sessionLength, setSessionLength] = useState(25)
-  const [timeLeft, setTimeLeft] = useState({minutes: sessionLength, seconds: 0})
-  const timerInterval = useRef(null)
-  const audio = useRef(null)
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [timeLeft, setTimeLeft] = useState({
+    minutes: sessionLength,
+    seconds: 0,
+  });
+  const timerInterval = useRef(null);
+  const audio = useRef(null);
 
   function handleStartStop(length) {
-    if(running===false){
-      running = true
-      start = Date.now()
-    } else running = false
-    timerFunction(length)
+    let start;
+    if (running === false) {
+      running = true;
+      start = Date.now();
+    } else running = false;
+    timerFunction(length);
   }
   function timerFunction(length) {
-    if(running===true){
-      if(switched===true){
-        totalTimeLeft = length*60 //remember to convert minute to seconds
-        switched = false
+    let minutes, seconds, totalTimeInSeconds;
+    if (running === true) {
+      if (switched === true) {
+        totalTimeInSeconds = length * 60; //remember to convert minute to seconds
+        switched = false;
       } else {
-        totalTimeLeft = (timeLeft.minutes*60) + timeLeft.seconds
+        totalTimeInSeconds = timeLeft.minutes * 60 + timeLeft.seconds;
       }
-      timerInterval.current = setInterval(() => {
-        time = totalTimeLeft - (((Date.now() - start)/1000).toFixed(0))
-        minutes = Math.floor(time/60)
-        seconds = time-(minutes*60)
-        setTimeLeft(
-          {
-            minutes: minutes,
-            seconds: seconds
-          }
-        )
-        if(time<=0){
-          clearInterval(timerInterval.current)
-          audio.current.play()
-          running = false
-          setTimeout(() => {
-            switchCurrentState(sessionLength, breakLength)
-          }, 2000);
+      let interval = 1000; // ms
+      let expected = Date.now() + interval;
+      timerInterval.current = setInterval(accurateIntervalFix, interval);
+
+      function accurateIntervalFix() {
+        let dt = Date.now() - expected; // the drift (positive for overshooting)
+        if (dt > interval) {
+          // Write something if you want to catch dt > interval
+          // possibly special handling to avoid futile "catch up" run
+          // alert("something something")
         }
-      }, 200);
+        // code between the accurate interval fix starts here
+        totalTimeInSeconds -= 1;
+        minutes = Math.floor(totalTimeInSeconds / 60);
+        seconds = totalTimeInSeconds - minutes * 60;
+        setTimeLeft({
+          minutes: minutes,
+          seconds: seconds,
+        });
+        if (totalTimeInSeconds === 0) {
+          clearInterval(timerInterval.current);
+          audio.current.play();
+          running = false;
+          setTimeout(() => {
+            switchCurrentState(sessionLength, breakLength);
+          }, 1000);
+        }
+        // code between the accurate interval fix ends here
+
+        expected += interval;
+        interval = Math.max(0, interval - dt); // take into account drift
+      }
     } else {
       clearInterval(timerInterval.current);
     }
   }
   function handleReset() {
-    audio.current.pause()
-    audio.current.currentTime = 0
+    audio.current.pause();
+    audio.current.currentTime = 0;
     setBreakLength(5);
     setSessionLength(25);
-    setTimeLeft({minutes: 25, seconds: 0})
-    if(running===true){handleStartStop(sessionLength)};
-    currentState = "Session"
+    setTimeLeft({ minutes: 25, seconds: 0 });
+    if (running === true) {
+      handleStartStop(sessionLength);
+    }
+    currentState = "Session";
   }
   function breakDecrement() {
-    if(breakLength>1 && running===false){
-      setBreakLength(prev=>prev-1)
-      if(currentState==="Break"){
-        setTimeLeft({minutes: breakLength-1, seconds: 0})
+    if (breakLength > 1 && running === false) {
+      setBreakLength((prev) => prev - 1);
+      if (currentState === "Break") {
+        setTimeLeft({ minutes: breakLength - 1, seconds: 0 });
       }
     }
   }
   function breakIncrement() {
-    if(breakLength<60 && running===false){
-      setBreakLength(prev=>prev+1)
-      if(currentState==="Break"){
-        setTimeLeft({minutes: breakLength+1, seconds: 0})
+    if (breakLength < 60 && running === false) {
+      setBreakLength((prev) => prev + 1);
+      if (currentState === "Break") {
+        setTimeLeft({ minutes: breakLength + 1, seconds: 0 });
       }
     }
   }
   function sessionDecrement() {
-    if(sessionLength>1 && running===false){
-      setSessionLength(prev=>prev-1)
-      if(currentState==="Session"){
-        setTimeLeft({minutes: sessionLength-1, seconds: 0})
+    if (sessionLength > 1 && running === false) {
+      setSessionLength((prev) => prev - 1);
+      if (currentState === "Session") {
+        setTimeLeft({ minutes: sessionLength - 1, seconds: 0 });
       }
     }
   }
   function sessionIncrement() {
-    if(sessionLength<60 && running===false){
-      setSessionLength(prev=>prev+1)
-      if(currentState==="Session"){
-        setTimeLeft({minutes: sessionLength+1, seconds: 0})
+    if (sessionLength < 60 && running === false) {
+      setSessionLength((prev) => prev + 1);
+      if (currentState === "Session") {
+        setTimeLeft({ minutes: sessionLength + 1, seconds: 0 });
       }
     }
-    
   }
   function switchCurrentState(sessionLengthParameter, breakLengthParameter) {
     switched = true;
-    if(currentState==="Session"){
-      currentState = "Break"
-      setTimeLeft({minutes: breakLengthParameter, seconds: 0})
-      handleStartStop(breakLengthParameter)
+    if (currentState === "Session") {
+      currentState = "Break";
+      setTimeLeft({ minutes: breakLengthParameter, seconds: 0 });
+      handleStartStop(breakLengthParameter);
     } else {
-      currentState = "Session"
-      setTimeLeft({minutes: sessionLengthParameter, seconds: 0})
-      handleStartStop(sessionLengthParameter)
+      currentState = "Session";
+      setTimeLeft({ minutes: sessionLengthParameter, seconds: 0 });
+      handleStartStop(sessionLengthParameter);
     }
   }
-  
 
   return (
     <div className="App">
       <div id="break-label">{"Break Length"}</div>
-      <button id="break-decrement" onClick={breakDecrement}>-</button>
+      <button id="break-decrement" onClick={breakDecrement}>
+        -
+      </button>
       <div id="break-length">{breakLength}</div>
-      <button id="break-increment" onClick={breakIncrement}>+</button>
+      <button id="break-increment" onClick={breakIncrement}>
+        +
+      </button>
       <div id="session-label">{"Session Length"}</div>
-      <button id="session-decrement" onClick={sessionDecrement}>-</button>
+      <button id="session-decrement" onClick={sessionDecrement}>
+        -
+      </button>
       <div id="session-length">{sessionLength}</div>
-      <button id="session-increment" onClick={sessionIncrement}>+</button>
+      <button id="session-increment" onClick={sessionIncrement}>
+        +
+      </button>
 
       <div id="timer-label">{currentState}</div>
-      <div id="time-left">{timeLeft.minutes<10 && 0}{timeLeft.minutes}{":"}{timeLeft.seconds<10 && 0}{timeLeft.seconds}</div>
-      <audio id="beep" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" ref={audio}/>
+      <div id="time-left">
+        {timeLeft.minutes < 10 && 0}
+        {timeLeft.minutes}
+        {":"}
+        {timeLeft.seconds < 10 && 0}
+        {timeLeft.seconds}
+      </div>
+      <audio
+        id="beep"
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        ref={audio}
+      />
 
-
-      <button id="start_stop" onClick={()=>handleStartStop(sessionLength)}>Start Stop</button>
-      <button id="reset" onClick={handleReset}>reset</button>
+      <button id="start_stop" onClick={handleStartStop}>
+        Start Stop
+      </button>
+      <button id="reset" onClick={handleReset}>
+        reset
+      </button>
     </div>
   );
 }
