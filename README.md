153132a0d8bafbac4ad6d89fb8728e73a5eced2c
agubucu <aemirozbek@gmail.com>
2023-07-26 16:36:35 +0300

    Styling and final improvements

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-25 + 5 (Pomodoro) Clock Application
+# Pomodoro Timer (25 + 5 Clock)
+
+This is a simple Pomodoro Timer app implemented using React, which allows users to set a session length and a break length. The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. It uses a timer to break work into intervals, traditionally 25 minutes in length, separated by short breaks.
+
+## Installation
+
+1. To run the Pomodoro Timer locally, follow these steps:
+1. Clone the repository or download the source code.
+1. Open a terminal and navigate to the project directory.
+1. Install the dependencies by running the command: npm install
+1. Start the development server with the command: npm start
+1. Open your web browser and visit http://localhost:3000 to see the application running.
+
+## Features
+
+- Set the session length: The user can set the duration of the work session (session length) using the "Session Length" buttons.
+- Set the break length: The user can set the duration of the break period (break length) using the "Break Length" buttons.
+- Timer Display: The app displays a countdown timer that shows the remaining time for the current session or break.
+- Start/Stop: The user can start or stop the timer by clicking on the "Play" (when paused) or "Pause" (when running) button.
+- Reset: The user can reset the timer and session/break lengths to their initial values by clicking on the "Reset" button.
+- Audio Alert: When the timer reaches zero, an audio alert will play to signal the end of a session or break.
+
+## About the Code
+
+The Pomodoro Timer is built using React and the way that the countdown logic works is a setInterval() method that calculates the drifted milliseconds that normally comes with the setInterval() method itself. It uses the interval value minus the previous drift value. So the drifting could be avoided. Special thanks to Bergi (https://stackoverflow.com/users/1048572/bergi) from stackoverflow, who posted the interval drift solution in https://stackoverflow.com/questions/29971898/how-to-create-an-accurate-timer-in-javascript. The method used in this code is inspired by him/her.
+
+## Contributing
+
+If you find any issues or have suggestions for improvements, feel free to open an issue or submit a pull request. Your contributions are welcome!
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import "https://kit.fontawesome.com/32ae4d2913.js";
 
   let running = false
   let switched = false
@@ -6,6 +7,7 @@ import { useState, useRef } from "react";
   let start, time, minutes, seconds, totalTimeLeft
 
 function App() {
+<<<<<<< Updated upstream
   const [breakLength, setBreakLength] = useState(5)
   const [sessionLength, setSessionLength] = useState(25)
   const [timeLeft, setTimeLeft] = useState({minutes: sessionLength, seconds: 0})
@@ -24,6 +26,30 @@ function App() {
       if(switched===true){
         totalTimeLeft = length*60 //remember to convert minute to seconds
         switched = false
+=======
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [timeLeft, setTimeLeft] = useState({
+    minutes: sessionLength,
+    seconds: 0,
+  });
+  const [play, setPlay] = useState(false);
+  const timerInterval = useRef(null);
+  const audio = useRef(null);
+
+  function handleStartStop(length) {
+    if (!running) {
+      running = true;
+    } else running = false;
+    timerFunction(length);
+  }
+  function timerFunction(length) {
+    let minutes, seconds, totalTimeInSeconds;
+    if (running) {
+      if (switched) {
+        totalTimeInSeconds = length * 60; //remember to convert minute to seconds
+        switched = false;
+>>>>>>> Stashed changes
       } else {
         totalTimeLeft = (timeLeft.minutes*60) + timeLeft.seconds
       }
@@ -55,6 +81,7 @@ function App() {
     audio.current.currentTime = 0
     setBreakLength(5);
     setSessionLength(25);
+<<<<<<< Updated upstream
     setTimeLeft({minutes: 25, seconds: 0})
     if(running===true){handleStartStop(sessionLength)};
     currentState = "Session"
@@ -64,30 +91,65 @@ function App() {
       setBreakLength(prev=>prev-1)
       if(currentState==="Break"){
         setTimeLeft({minutes: breakLength-1, seconds: 0})
+=======
+    setTimeLeft({ minutes: 25, seconds: 0 });
+    setPlay(false);
+    if (running) {
+      handleStartStop(sessionLength);
+    }
+    currentState = "Session";
+  }
+  function breakDecrement() {
+    if (breakLength > 1 && !running) {
+      setBreakLength((prev) => prev - 1);
+      if (currentState === "Break") {
+        setTimeLeft({ minutes: breakLength - 1, seconds: 0 });
+>>>>>>> Stashed changes
       }
     }
   }
   function breakIncrement() {
+<<<<<<< Updated upstream
     if(breakLength<60 && running===false){
       setBreakLength(prev=>prev+1)
       if(currentState==="Break"){
         setTimeLeft({minutes: breakLength+1, seconds: 0})
+=======
+    if (breakLength < 60 && !running) {
+      setBreakLength((prev) => prev + 1);
+      if (currentState === "Break") {
+        setTimeLeft({ minutes: breakLength + 1, seconds: 0 });
+>>>>>>> Stashed changes
       }
     }
   }
   function sessionDecrement() {
+<<<<<<< Updated upstream
     if(sessionLength>1 && running===false){
       setSessionLength(prev=>prev-1)
       if(currentState==="Session"){
         setTimeLeft({minutes: sessionLength-1, seconds: 0})
+=======
+    if (sessionLength > 1 && !running) {
+      setSessionLength((prev) => prev - 1);
+      if (currentState === "Session") {
+        setTimeLeft({ minutes: sessionLength - 1, seconds: 0 });
+>>>>>>> Stashed changes
       }
     }
   }
   function sessionIncrement() {
+<<<<<<< Updated upstream
     if(sessionLength<60 && running===false){
       setSessionLength(prev=>prev+1)
       if(currentState==="Session"){
         setTimeLeft({minutes: sessionLength+1, seconds: 0})
+=======
+    if (sessionLength < 60 && !running) {
+      setSessionLength((prev) => prev + 1);
+      if (currentState === "Session") {
+        setTimeLeft({ minutes: sessionLength + 1, seconds: 0 });
+>>>>>>> Stashed changes
       }
     }
     
@@ -107,6 +169,7 @@ function App() {
   
 
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <div id="break-label">{"Break Length"}</div>
       <button id="break-decrement" onClick={breakDecrement}>-</button>
@@ -124,6 +187,89 @@ function App() {
 
       <button id="start_stop" onClick={()=>handleStartStop(sessionLength)}>Start Stop</button>
       <button id="reset" onClick={handleReset}>reset</button>
+=======
+    <div id="container">
+      <div id="title">Pomodoro Timer</div>
+      <div className="App">
+        <div id="timer-container">
+          <div id="timer-label">{currentState}</div>
+          <div id="time-left">
+            {timeLeft.minutes < 10 && 0}
+            {timeLeft.minutes}
+            {":"}
+            {timeLeft.seconds < 10 && 0}
+            {timeLeft.seconds}
+          </div>
+        </div>
+        <div id="start-reset">
+          <button
+            id="start_stop"
+            onClick={() => {
+              handleStartStop();
+              setPlay((prev) => (prev ? false : true));
+            }}
+          >
+            {play ? (
+              <i className="fa-solid fa-pause"></i>
+            ) : (
+              <i className="fa-solid fa-play"></i>
+            )}
+          </button>
+          <button id="reset" onClick={handleReset}>
+            <i className="fa-solid fa-reply"></i>
+          </button>
+        </div>
+        <div id="break-container">
+          <div id="break-label">{"Break Length"}</div>
+          <button
+            id="break-decrement"
+            className="increment-decrement"
+            onClick={breakDecrement}
+          >
+            <i className="fa-solid fa-minus"></i>
+          </button>
+          <div id="break-length">{breakLength}</div>
+          <button
+            id="break-increment"
+            className="increment-decrement"
+            onClick={breakIncrement}
+          >
+            <i className="fa-solid fa-plus"></i>
+          </button>
+        </div>
+        <div id="session-container">
+          <div id="session-label">{"Session Length"}</div>
+          <button
+            id="session-decrement"
+            className="increment-decrement"
+            onClick={sessionDecrement}
+          >
+            <i className="fa-solid fa-minus"></i>
+          </button>
+          <div id="session-length">{sessionLength}</div>
+          <button
+            id="session-increment"
+            className="increment-decrement"
+            onClick={sessionIncrement}
+          >
+            <i className="fa-solid fa-plus"></i>
+          </button>
+        </div>
+
+        <audio
+          id="beep"
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+          ref={audio}
+        />
+      </div>
+      <div id="author">
+        Designed and Coded by
+        <br />
+        <a href="https://www.freecodecamp.org/ozbek" target="_blank">
+          Emir Özbek
+        </a>
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
